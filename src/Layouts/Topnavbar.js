c84fd2e7d601ebb6340d8678c0e7b6197d74783e
Emmanuel Oladimeji Adeyemi <46575938+yemicodes@users.noreply.github.com>
2022-05-30 00:11:41 +0300

    add top navbar for responsive build

--- src/Layouts/Topnavbar.js
+++ src/Layouts/Topnavbar.js
@@ -1,7 +1,21 @@
 import React from "react";
 import "./Layout.css";
+import "./Topnavbar.css";
 function Topnavbar() {
-  return <div className="topnavbar">Top navbar</div>;
+  return <div className="topnavbar">
+      <div className="topnavbar__body">
+          <div className="topnavbar__img">
+              <img
+            src="https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687"
+            alt="User"
+            className="topnav__img"
+          /> 
+          </div>
+          
+      </div>
+     
+
+  </div>;
 }
 
 export default Topnavbar;
