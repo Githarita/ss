68874b16c42326e2b51aaf8b80979634f1b10352
Emmanuel Oladimeji Adeyemi <46575938+yemicodes@users.noreply.github.com>
2022-06-04 19:24:56 +0300

    Update Topnavbar.js

--- src/Layouts/Topnavbar.js
+++ src/Layouts/Topnavbar.js
@@ -11,9 +11,9 @@ function Topnavbar() {
     let lastScrollY = window.scrollY;
     window.addEventListener("scroll", () => {
       if (lastScrollY < window.scrollY) {
-        setHideNav("topnavbar--hide");
-      } else {
         setHideNav("topnavbar");
+      } else {
+        setHideNav("topnavbar--hide");
       }
 
       lastScrollY = window.scrollY;
