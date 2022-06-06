b08e316874e5fcea0ed2bd1fb8a582e42402d7ee
Emmanuel Oladimeji Adeyemi <46575938+yemicodes@users.noreply.github.com>
2022-06-06 15:49:32 +0300

    working on the notification

--- src/Pages/Notificationpage/Notificationpage.js
+++ src/Pages/Notificationpage/Notificationpage.js
@@ -1,20 +1,55 @@
-import React from 'react'
-import Body from '../../Layouts/Body';
-import Bottombar from '../../Layouts/Bottombar';
-import Mainlayout from '../../Layouts/Mainlayout';
-import Sidebar from '../../Layouts/Sidebar';
-import Topnavbar from '../../Layouts/Topnavbar';
+import React from "react";
+import Body from "../../Layouts/Body";
+import Bottombar from "../../Layouts/Bottombar";
+import Mainlayout from "../../Layouts/Mainlayout";
+import Sidebar from "../../Layouts/Sidebar";
+import Topnavbar from "../../Layouts/Topnavbar";
 import "./Notificationpage.css";
+import image1 from "../../assets/shoe1.png";
+import image2 from "../../assets/shoe2.png";
+import image3 from "../../assets/shoe3.png";
 
 function Notificationpage() {
   return (
     <Mainlayout>
-        <Topnavbar/>
-        <Sidebar />
-        <Body></Body>
-        <Bottombar />
+      <Topnavbar />
+      <Sidebar />
+      <Body>
+        <div className="notificationbody">
+          <div className="notificationitem">
+            <img src={image1} alt="" />
+            <div className="notification_typography">
+              <p className="notificationheader">Bid match available</p>
+              <p className="notificationparagraph">
+                Your Bid on Nike Air Zoom Terra for ₦70,000.00 have been found.
+              </p>
+            </div>
+          </div>
+          <div className="notificationitem">
+            <img src={image2} alt="" />
+            <div className="notification_typography">
+              <p className="notificationheader">Ask match available</p>
+              <p className="notificationparagraph">
+                Your Ask on Nike Air Zoom Terra for ₦70,000.00 have been found.
+                and that is how we see it. so don't give us trouble when the
+                notifications are coming in
+              </p>
+            </div>
+          </div>
+          <div className="notificationitem">
+            <img src={image3} alt="" />
+            <div className="notification_typography">
+              <p className="notificationheader">Bid match available</p>
+              <p className="notificationparagraph">
+                Your Bid on Nike Air Zoom Terra for ₦70,000.00 have been found.
+              </p>
+            </div>
+          </div>
+        </div>
+      </Body>
+      <Bottombar />
     </Mainlayout>
-  )
+  );
 }
 
-export default Notificationpage
+export default Notificationpage;
